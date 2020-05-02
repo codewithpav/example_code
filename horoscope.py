71ba2e03cc68ed2d49349e2c981f9d94a336b0e0
codewithpav <62837426+codewithpav@users.noreply.github.com>
2020-05-03 00:20:22 +0800

    Create horoscope.py

---
+++ horoscope.py
@@ -0,0 +1,9 @@
+months = ['January','February', 'March', 'Aprul','May','June','July','August','September','October','November','December']
+
+horiscope_dict = {'January': 'Aries', 'February': 'Taurus', 'March': 'Gemini', 'Aprul': 'Cancer', 'May': 'Leo', 'June': 'Virgo', 'July': 'Libra', 'August': 'Scorpio', 'September': 'Sagittarius', 'October': 'Capricorn', 'November': 'Aquarius', 'December': 'Pisces'}
+
+user_month = input("Which month are you born in?")
+
+if user_month in horiscope_dict:
+    x = horiscope_dict[user_month]
+    print(f"Your star sign might be {x}")
