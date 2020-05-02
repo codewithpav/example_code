2072383549a30f62755f09d8a94efced529ca4ab
codewithpav <62837426+codewithpav@users.noreply.github.com>
2020-05-03 00:20:47 +0800

    Update horoscope.py

--- horoscope.py
+++ horoscope.py
@@ -1,5 +1,3 @@
-months = ['January','February', 'March', 'Aprul','May','June','July','August','September','October','November','December']
-
 horiscope_dict = {'January': 'Aries', 'February': 'Taurus', 'March': 'Gemini', 'Aprul': 'Cancer', 'May': 'Leo', 'June': 'Virgo', 'July': 'Libra', 'August': 'Scorpio', 'September': 'Sagittarius', 'October': 'Capricorn', 'November': 'Aquarius', 'December': 'Pisces'}
 
 user_month = input("Which month are you born in?")
